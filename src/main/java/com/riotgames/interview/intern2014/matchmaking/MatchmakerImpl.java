3cd00da09b96755c7133975bca5efccdb7a65062
Riccardo Mutschlechner <riccardo@wifi-ls-74-148.resnet.wisc.edu>
2014-02-16 00:00:19 -0600

    Queue system works! I think....

--- src/main/java/com/riotgames/interview/intern2014/matchmaking/MatchmakerImpl.java
+++ src/main/java/com/riotgames/interview/intern2014/matchmaking/MatchmakerImpl.java
@@ -1,32 +1,61 @@
 package com.riotgames.interview.intern2014.matchmaking;
 
+import java.util.HashSet;
+import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.Queue;
+import java.util.Set;
 
 /**
  * The matchmaking implementation that you will write.
  */
 public class MatchmakerImpl implements Matchmaker {
-	
+
 	private Queue<Player> matchmakingQueue;
-	
+
 	public MatchmakerImpl(){
 		this.matchmakingQueue = new LinkedList<Player>();
 	}
 
-    public Match findMatch(int playersPerTeam) {
-        // TODO be sure to implement this :D
-        return null;
-    }
+	public Match findMatch(int playersPerTeam) {
+		//Set up our two teams as HashSets
+		Set<Player> team1 = new HashSet<Player>();
+		Set<Player> team2 = new HashSet<Player>();
+		
+		//Set up our variables
+		int playersInTeam1 = 0; //Our team1 counter
+		int playersInTeam2 = 0; //Our team2 counter
+		
+		double tolerance = 0.05; //our tolerance for matchmaking
+		
+		Iterator<Player> itr = this.matchmakingQueue.iterator();
+		Player p1 = null, p2 = null;
+		
+		while(itr.hasNext()){
+			p1 = itr.next();
+			
+			if(itr.hasNext())
+				p2 = itr.next();
+			
+			if(p1 != null && p2 != null){
+				if(p1.isCompatibleWith(p2, tolerance)){
+					team1.add(p1); 
+					team1.add(p2);
+				}
+			}
+		}
+		
+		return new Match(team1, team2);
+	}
 
-    public void enterMatchmaking(Player player) {
-        // TODO fully finish this
-    	//Add the player into the Matchmaking Queue
-    	this.matchmakingQueue.offer(player);
-    }
-    
-    public Queue<Player> getQueue(){
-    	return this.matchmakingQueue;
-    }
+	public void enterMatchmaking(Player player) {
+		// TODO fully finish this
+		//Add the player into the Matchmaking Queue
+		this.matchmakingQueue.offer(player);
+	}
+
+	public Queue<Player> getQueue(){
+		return this.matchmakingQueue;
+	}
 
 }
--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -1,3 +1,4 @@
+import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Queue;
@@ -14,8 +15,11 @@ public class Test {
 		MatchmakerImpl mmi = new MatchmakerImpl();
 		
 		//Create two dummy players 
-		Player p1 = new Player("Ricky", 75, 100); //WLR 0.75
+		Player p1 = new Player("Ricky", 80, 100); //WLR 0.80
 		Player p2 = new Player("Krissy", 85, 100); //WLR 0.85
+		Player p3 = new Player("Topkek", 85, 100); //WLR 0.85
+		Player p4 = new Player("Toplel", 80, 100); // WLR 0.80
+		Player p5 = new Player("Tophue", 85, 100); //WLR 0.85
 
 		//Add the first three to the matchmaking system
 //		mmi.enterMatchmaking(players.get(0));
@@ -23,23 +27,30 @@ public class Test {
 //		mmi.enterMatchmaking(players.get(2));
 		mmi.enterMatchmaking(p1);
 		mmi.enterMatchmaking(p2);
-		
-		
-		//See if p1's WLR is within 10% (inclusive) of p2's WLR,
-		//if so, they are compatible. If not, they are not.
-		System.out.println(p1.isCompatibleWith(p2, 0.10));
-		
+		mmi.enterMatchmaking(p3);
+		mmi.enterMatchmaking(p4);
+		mmi.enterMatchmaking(p5);
 		
 		Queue<Player> queue = mmi.getQueue();
 
 		//Show all players that are in the queue currently
-		Iterator<Player> itr = queue.iterator();
-		Player p;
-		while(itr.hasNext()){
-			p = itr.next();
-			System.out.print(p);
-			System.out.println(" WLR: " + p.getWLR());
+//		Iterator<Player> itr = queue.iterator();
+//		Player p;
+//		while(itr.hasNext()){
+//			p = itr.next();
+//			System.out.print(p);
+//			System.out.printf(" WLR: %1.2f\n", p.getWLR());
+//		}
+		
+		//Attempt to match up 5 players; p1 -> p5 should be matched
+		Match m = mmi.findMatch(5);
+		HashSet<Player> team1 = (HashSet<Player>) m.getTeam1();
+		
+		System.out.println("***TEAM 1:***");
+		for(Player player: team1){
+			System.out.println(player);
 		}
+		
 	}
 
 }
